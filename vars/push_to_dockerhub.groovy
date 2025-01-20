9f621a682e113f38b8f9e233eb15e2500bf7dd31
poojaprakash0927 <67591524+poojaprakash0927@users.noreply.github.com>
2025-01-19 19:16:36 -0800

    corrected push to dockerfile

--- vars/push_to_dockerhub.groovy
+++ vars/push_to_dockerhub.groovy
@@ -1,6 +1,14 @@
-def call(String dockerHubUser, String dockerHubPass, String image_name, String tag) {
+def call(String image_name, String tag) {
+        withCredentials([usernamePassword(
+                    credentialsId:"dockerhub_credentials",
+                    usernameVariable:"dockerHubUser", 
+                    passwordVariable:"dockerHubPass")]){
+                        push_to_dockerhub("dockerHubUser", "dockerHubPass", 'simple-flask-app', 'v2')
         sh "docker login -u $dockerHubUser -p $dockerHubPass"
         sh "docker image tag $image_name:$tag $dockerHubUser/$image_name:$tag"
         sh "docker push $dockerHubUser/$image_name:$tag"
         echo "pushed image to docker hub"
+        
+        }
+        
 }
