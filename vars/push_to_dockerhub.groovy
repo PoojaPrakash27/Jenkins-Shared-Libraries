0d0375a00dd172385a049f0e5b8ca2f411f59600
poojaprakash0927 <67591524+poojaprakash0927@users.noreply.github.com>
2025-01-19 19:24:45 -0800

    removed incorrect function call from push_to_dockerhub

--- vars/push_to_dockerhub.groovy
+++ vars/push_to_dockerhub.groovy
@@ -3,7 +3,6 @@ def call(String image_name, String tag) {
                     credentialsId:"dockerhub_credentials",
                     usernameVariable:"dockerHubUser", 
                     passwordVariable:"dockerHubPass")]){
-                        push_to_dockerhub("dockerHubUser", "dockerHubPass", 'simple-flask-app', 'v2')
         sh "docker login -u $dockerHubUser -p $dockerHubPass"
         sh "docker image tag $image_name:$tag $dockerHubUser/$image_name:$tag"
         sh "docker push $dockerHubUser/$image_name:$tag"
